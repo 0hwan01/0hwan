614ae394991da696d1ae1219926da438c27bcd17
hwany <75780275+0hwan01@users.noreply.github.com>
2023-10-15 18:40:24 +0900

    Add files via upload

---
+++ momentum/js/background.js
@@ -0,0 +1,10 @@
+const images = ["0img.jpg", "1img.jpg", "2img.jpg",];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
+
